d99a78b00037467c6d5794b6f4f1e5589e583286
Xensen008 <arnabjyotikakati008@gmail.com>
2024-01-27 18:37:24 +0530

    day 18 code file updated

--- Day 18/code18.cpp
+++ Day 18/code18.cpp
@@ -20,16 +20,11 @@ private:
 };
 
 int main() {
-    // Using std::unique_ptr
+
     std::cout << "Using std::unique_ptr:" << std::endl;
     std::unique_ptr<Resource> uniquePtr = std::make_unique<Resource>(42);
     uniquePtr->processData();
 
-    // Uncommenting the line below will result in a compilation error,
-    // demonstrating unique ownership.
-    // std::unique_ptr<Resource> anotherUniquePtr = uniquePtr; 
-
-    // Using std::shared_ptr
     std::cout << "\nUsing std::shared_ptr:" << std::endl;
     std::shared_ptr<Resource> sharedPtr1 = std::make_shared<Resource>(42);
     std::shared_ptr<Resource> sharedPtr2 = sharedPtr1;
